1c800fd4d7377b6e8ccf4d23f8722e1741a01a57
Lamin Barrow <laminbarrow@gmail.com>
2024-08-16 16:41:51 +0000

    feat: Add /info endpoint to display phonebook information
    The code changes add a new endpoint `/info` to the application. This endpoint displays the number of people in the phonebook and the current date.

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+app.get('/info', (req, res) => {
+    const date = new Date();
+    res.send(`
+        <p>Phonebook has info for ${notes.length} people</p>
+        <p>${date}</p>
+    `);
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(notes);
 })
