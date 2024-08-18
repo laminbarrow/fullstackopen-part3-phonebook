51ba18bbbabec67be0833ffc15cac615214d2a50
Lamin Barrow <laminbarrow@gmail.com>
2024-08-18 19:04:36 +0000

    feat: Configure morgan to show data sent in HTTP  requests
    The code changes modify the configuration of the morgan middleware to include the data sent in HTTP POST requests. This is achieved by adding a custom token `post-body` that returns the JSON stringified version of the request body. The updated configuration is applied to the Express app using the `app.use` method.

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const morgan = require('morgan')
 
 // middleware uses
 app.use(express.json())
-app.use(morgan('tiny'))
+
+//Configure morgan so that it also shows the data sent in HTTP POST requests:
+morgan.token('post-body', function (req, res) { 
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
 const PORT = 3001
 
